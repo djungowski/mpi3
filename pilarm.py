dfd383db63f93bc39b53a6aa88407318c71b768e
Dominik Jungowski <Dominik@Jungowski.de>
2013-03-19 22:49:12 +0100

    First audioplay

--- pilarm.py
+++ pilarm.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python
 
-import os, sys, logging, pygame
+import os, sys, logging
+
+from mplayer import Player
+
+player = Player()
+player.loadfile('../mp3/good_charlotte_-_we_believe__matthew_adams_true_sadness_mix.mp3')
+
+def input():
+	logging.debug("Foobar")
+
+while True:
+	input()
---
+++ setup.sh
@@ -0,0 +1,16 @@
+echo "+++ Installing mplayer"
+sudo apt-get install mplayer
+
+echo "+++ Creating setup dir"
+mkdir setup
+cd setup
+
+echo "+++ Installing python-mplayer"
+git clone git://github.com/baudm/mplayer.py.git
+cd mplayer.py
+sudo python setup.py install
+
+echo "+++ Removing setup dir"
+cd ../
+cd ../
+sudo rm -rf setup
