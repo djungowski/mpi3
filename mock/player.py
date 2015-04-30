acd69345b5a5ee5047fb66a64f92ebfac903b34b
Dominik Jungowski <Dominik@Jungowski.de>
2015-04-30 21:30:13 +0200

    Mocking logging

--- mock/player.py
+++ mock/player.py
@@ -1,8 +1,13 @@
 class Player:
 	loop = 0
+	__logging = None
 
 	def __init__(self, logging):
+		self.__logging = logging
 		logging.info("Starting Player mock")
 
 	def repeat(self, howOften):
 		self.loop = howOften
+
+	def quit(self):
+		self.__logging.debug("Shutting down player mock")
