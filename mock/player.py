b21a3e869a6e43893eae1a55093eafa02b4cbb39
Dominik Jungowski <Dominik@Jungowski.de>
2015-04-30 21:35:18 +0200

    Full mock

--- mock/player.py
+++ mock/player.py
@@ -4,10 +4,22 @@ class Player:
 
 	def __init__(self, logging):
 		self.__logging = logging
-		logging.info("Starting Player mock")
+		logging.info('[Player Mock] Starting Player mock')
+
+	def play(self, filename):
+		self.__logging.info('[Player Mock] Playing ' + filename)
+
+	def stop(self):
+		self.__logging.info('[Player Mock] Stopping Playback')
+
+	def pause(self):
+		self.__logging.info('[Player Mock] Pausing playback')
 
 	def repeat(self, howOften):
 		self.loop = howOften
 
+	def fadein(self, filename):
+		self.__logging.info('[Player Mock] Fading in ' + filename)
+
 	def quit(self):
-		self.__logging.debug("Shutting down player mock")
+		self.__logging.info('[Player Mock] Shutting down player mock')
